e73a2a8303b24aa30e393546d666dee7145e698f
Omkar081 <715striker@gmail.com>
2022-03-08 10:31:09 -0600

    changed everything from phoenix to REV

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,6 +7,12 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,45 +21,49 @@ import frc.robot.Constants.MotorIDConstants;
 
 public class ClimbSubsystem extends SubsystemBase {
   /** Creates a new ClimbSubsystem. */
-  WPI_TalonSRX upperClimbMotor = new WPI_TalonSRX(MotorIDConstants.upperClimbMotorID);
-  WPI_VictorSPX lowerClimbMotor = new WPI_VictorSPX(MotorIDConstants.lowerClimbMotorID);
+  CANSparkMax rightLifter = new CANSparkMax(MotorIDConstants.rightClimbMotorID, MotorType.kBrushless);
+  CANSparkMax leftLifter = new CANSparkMax(MotorIDConstants.leftClimbMotorID, MotorType.kBrushless);
 
-  MotorControllerGroup climbMotors = new MotorControllerGroup(upperClimbMotor, lowerClimbMotor);
+
+  SparkMaxPIDController pidController = rightLifter.getPIDController(); //instantiate the PID of the rightClimbMotor
+  RelativeEncoder rightEncoder = rightLifter.getEncoder(); //instaitiate the encoder of the rightClimbMotor
 
   public ClimbSubsystem() {
-    lowerClimbMotor.follow(upperClimbMotor);
-    upperClimbMotor.selectProfileSlot(0, 0); //may need to change this idk though
-    upperClimbMotor.config_kP(0, 0);
-    upperClimbMotor.config_kI(0, 0);
-    upperClimbMotor.config_kD(0, 0);
+    leftLifter.follow(rightLifter);
+    pidController.setP(0);
+    pidController.setI(0);
+    pidController.setD(0);
   }
-
- 
-  public void climbUp() {
-    upperClimbMotor.set(.5);
+  //try to use these Up and Down methods first before doing PID stuff
+  public void climbUp() { 
+    rightLifter.set(.5);
   }
 
   public void climbDown() {
-    upperClimbMotor.set(.5);
+    rightLifter.set(-.5);
+    
   }
   public void stopClimb() {
-    upperClimbMotor.set(0);
+    rightLifter.set(0);
   }
 
-  public double getClimbPosition() {
-    return upperClimbMotor.getSelectedSensorPosition();
+  public double getClimbPosition() { 
+    //postiton is measured in rotations (of motor I think, so it would be the same as ticks on a motor)
+    //rightEncoder.setPositionConversionFactor(factor) //you can use this to change the readings of position to a more sensible unit but I have no idea what number that would be
+    return rightEncoder.getPosition();
+
   }
 
-  public void climbToPosition(double position) {
-    upperClimbMotor.set(ControlMode.Position, position); //you may need to do this for lowerMotor but im not sure
+  public void climbToPosition(double position) { //postition is rotations of the motor(ticks)
+    pidController.setReference(position, ControlType.kSmartMotion); //smart motion is the built in PID of the NEOs
   }
 
   public void resetClimbPosition() {
-    upperClimbMotor.setSelectedSensorPosition(0);
+    rightEncoder.setPosition(0); 
   }
 
   public boolean isAtClimbHeight() {
-    return (Math.abs(upperClimbMotor.getClosedLoopError()) < ClimbConstants.positionTolerance);
+    return (Math.abs(pidController.getSmartMotionAllowedClosedLoopError(0)) < ClimbConstants.positionTolerance); //please look at this
   }
   @Override
   public void periodic() {
